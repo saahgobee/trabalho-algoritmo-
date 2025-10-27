35f1eefc6d421d1972a3722651d924b160c5ddcb
saahgobee <160123459+saahgobee@users.noreply.github.com>
2025-10-27 10:42:08 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 
+function mostrarMensagem() {
+  const nome = document.getElementById('nome').value;
+  const mensagem = document.getElementById('mensagem');
+  
+  if (nome.trim() === "") {
+    mensagem.textContent = "Por favor, digite seu nome!";
+  } else {
+    mensagem.textContent = `Olá ${nome}, é um prazer te conhecer!`;
+  }
+}
